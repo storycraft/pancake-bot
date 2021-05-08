c4cb59c8e7e89a0f04a48982d83be4da73c66eeb
storycraft <storycraft@pancake.sh>
2021-05-09 00:20:01 +0900

    [school-meal] 모듈이 제대로 작동하도록 픽스

--- modules/school-meal/index.ts
+++ modules/school-meal/index.ts
@@ -18,7 +18,7 @@ export const MODULE_DESC: ModuleDescriptor = {
 }
 
 export type ModuleOptions = {
-    apiKey: String;
+    apiKey: string;
 };
 
 export default function moduleInit(mod: BotModule, options: ModuleOptions) {
@@ -39,14 +39,14 @@ export default function moduleInit(mod: BotModule, options: ModuleOptions) {
     );
 }
 
-async function smCommand(apiKey: String, info: CommandInfo, ctx: TalkContext<TalkChannel>) {
+async function smCommand(apiKey: string, info: CommandInfo, ctx: TalkContext<TalkChannel>) {
     let schoolCode: string = ''; //표준학교코드
     let scCode: string = ''; //시도교육청코드
 
-    const args: string[] = info.args.split('\n');
+    const args: string[] = info.args.split(' ');
 
     try {
-        const res = await fetch(`https://open.neis.go.kr/hub/schoolInfo?KEY=${apiKey}&Type=json&SCHUL_NM=${args[0]}`);
+        const res = await fetch(`https://open.neis.go.kr/hub/schoolInfo?KEY=${encodeURIComponent(apiKey)}&Type=json&SCHUL_NM=${encodeURIComponent(args[0])}`);
         const resj = await res.json();
 
         schoolCode = resj.schoolInfo[1].row[0].SD_SCHUL_CODE;
@@ -63,15 +63,17 @@ async function smCommand(apiKey: String, info: CommandInfo, ctx: TalkContext<Tal
     try {
         let dayStr = '';
 
-        if(args.length > 0){
+        if(args.length < 2){
             const today = new Date();
-            dayStr = `${today.getFullYear()}${today.getMonth()+1}${today.getDate()}`;
+            dayStr = `${today.getFullYear()}${(today.getMonth()+1).toString().padStart(2, '0')}${today.getDate().toString().padStart(2, '0')}`;
         }else{
             dayStr = args[1];
         }
         
         const smData = await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?&KEY=${apiKey}&Type=json&pSize=1&SD_SCHUL_CODE=${schoolCode}&ATPT_OFCDC_SC_CODE=${scCode}&MLSV_YMD=${dayStr}`);
         const smDataj = await smData.json();
+
+        console.log(`https://open.neis.go.kr/hub/mealServiceDietInfo?&KEY=${apiKey}&Type=json&pSize=1&SD_SCHUL_CODE=${schoolCode}&ATPT_OFCDC_SC_CODE=${scCode}&MLSV_YMD=${dayStr}`);
 
         await ctx.channel.sendChat(smDataj.mealServiceDietInfo[1].row[0].DDISH_NM.replace(/<br\/>/g, '\n'));
     } catch(err) {
